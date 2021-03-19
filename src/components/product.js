8a64a7b748496c6c1e2dbd88854a0e661b4bc56f
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-19 15:35:43 +0200

    Add custom hook

--- src/components/product.js
+++ src/components/product.js
@@ -1,10 +1,17 @@
 import React from "react";
+import useAmount from "../hooks/use-amount";
 
 const Product = (props) => {
+  const { count, decrement, increment } = useAmount(0);
+
   return (
     <div>
       <p>{props.product.name}</p>
-      <p>{props.product.price}</p>
+      <p>{props.product.price}$</p>
+
+      <button onClick={decrement}>-</button>
+      {count}
+      <button onClick={increment}>+</button>
     </div>
   );
 };
---
+++ src/hooks/use-amount.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useAmount = (initialCount = 0) => {
+  const [count, setCount] = useState(initialCount);
+
+  const decrement = () => setCount(count > 0 ? count - 1 : 0);
+  const increment = () => setCount(count + 1);
+
+  return { count, decrement, increment };
+};
+
+export default useAmount;
