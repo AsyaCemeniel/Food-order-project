c6728c820683e5cd157e55e3b7809daf337d5c21
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-19 17:46:03 +0200

    Add icons

--- src/components/product.js
+++ src/components/product.js
@@ -1,5 +1,8 @@
 import React from "react";
 import counter from "../hocs/counter";
+//icons
+import minus from "../icons/minus.svg";
+import plus from "../icons/plus.svg";
 
 const Product = (props) => {
   const { count, decrement, increment } = props;
@@ -9,9 +12,13 @@ const Product = (props) => {
       <p>{props.product.name}</p>
       <p>{props.product.price}$</p>
 
-      <button onClick={decrement}>-</button>
+      <button onClick={decrement}>
+        <img src={minus} alt="minus" />
+      </button>
       {count}
-      <button onClick={increment}>+</button>
+      <button onClick={increment}>
+        <img src={plus} alt="plus" />
+      </button>
     </div>
   );
 };
