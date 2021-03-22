cf140bd9bbc186ac2821cb11adf04ef1f7221fcc
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-22 13:18:08 +0200

    Add tests

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -15,4 +15,20 @@ describe("Product", () => {
 
     expect(component.find('[data-id="product"]').length).toBe(1);
   });
+  it("should init with amount 0", () => {
+    const component = mount(<Product product={product} />);
+
+    expect(component.find('[data-id="product-amount"]').text()).toBe("0");
+  });
+  it("should increment amount", () => {
+    const component = mount(<Product product={product} />);
+    component.find('[data-id="product-increment"]').simulate("click");
+    expect(component.find('[data-id="product-amount"]').text()).toBe("1");
+  });
+  it("should fetch data", () => {
+    const fn = jest.fn();
+    mount(<Product product={product} fetchData={fn} />);
+
+    expect(fn).toBeCalledWith(product.id);
+  });
 });
