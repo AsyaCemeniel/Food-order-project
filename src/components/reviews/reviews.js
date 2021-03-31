b4f4a77511a255d5289494ff5eb10362210e9879
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-31 10:58:12 +0300

    add ReviewForm

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,22 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+// import ReviewForm from './review-form';
 import Review from "./review";
 import styles from "./reviews.module.css";
 
 const Reviews = ({ reviews }) => {
   return (
-    <div className={styles.reviews} data-id="reviews">
+    <div className={styles.reviews}>
       {reviews.map((review) => (
-        <Review key={review.id} {...review} data-id="review" />
+        <Review key={review.id} {...review} />
       ))}
+      {/* <ReviewForm /> */}
     </div>
   );
 };
 
 Reviews.propTypes = {
   reviews: PropTypes.arrayOf(
-    PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired
   ).isRequired,
 };
 
