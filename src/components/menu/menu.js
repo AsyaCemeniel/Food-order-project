ec00be533d40be75d17f02636aa01dfd8ceffc95
AsyaCemeniel <asyaorlov89@gmail.com>
2021-04-09 11:09:44 +0300

    Add HW5

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,6 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loadProducts } from "../../redux/actions";
+import {
+  productsLoadingSelector,
+  productsLoadedSelector,
+} from "../../redux/selectors";
 
+import Loader from "../loader";
 import Product from "../product";
 
 import styles from "./menu.module.css";
@@ -8,19 +15,42 @@ import Basket from "../basket";
 
 class Menu extends React.Component {
   static propTypes = {
-    menu: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+    menu: PropTypes.arrayOf(
+      PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired
+    ).isRequired,
   };
 
   state = {
     error: null,
   };
 
+  loadProductsIfNeeded = () => {
+    const { loadProducts, restaurantId, loading, loaded } = this.props;
+    if (!loading && !loaded) {
+      loadProducts(restaurantId);
+    }
+  };
+
+  componentDidMount() {
+    this.loadProductsIfNeeded();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.restaurantId !== this.props.restaurantId) {
+      this.loadProductsIfNeeded();
+    }
+  }
+
   componentDidCatch(error) {
     this.setState({ error });
   }
 
   render() {
-    const { menu } = this.props;
+    const { menu, loading } = this.props;
+
+    if (loading) {
+      return <Loader />;
+    }
 
     if (this.state.error) {
       return <h1>{this.state.error.message}</h1>;
@@ -41,4 +71,14 @@ class Menu extends React.Component {
   }
 }
 
-export default Menu;
+Menu.propTypes = {
+  menu: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
+
+export default connect(
+  (state, props) => ({
+    loading: productsLoadingSelector(state, props),
+    loaded: productsLoadedSelector(state, props),
+  }),
+  { loadProducts }
+)(Menu);
